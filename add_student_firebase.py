287ba8ad0dbd00b15481a4f020cac29652b60d11
hiengyen <ngyentrunghieu742003@gmail.com>
2025-01-11 21:30:25 +0700

    update ..

--- add_student_firebase.py
+++ add_student_firebase.py
@@ -9,8 +9,30 @@ firebase_admin.initialize_app(cred, {
 ref_students = db.reference("students")
 
 users = [
-    {"student_id": "CT060409", "student_name": "Tran Luu Dung",
-     "rfid_code": "1A2B3C4D"},
+    # {"student_id": "CT060409", "student_name": "Tran Luu Dung",
+    #  "rfid_code": "1A2B3C4D"},
+    {
+        "student_id": "Unknown",
+        "student_name": "Khong ton tai",
+        "rfid_code": "00000000",
+    },
+    # {
+    #     "student_id": "CT060412",
+    #     "student_name": "Nguyen Trung Hieu",
+    #     "rfid_code": "9FE9721C",
+    # },
+    # {
+    #     "student_id": "CT060406",
+    #     "student_name": "Nguyen Minh Duc ",
+    #     "rfid_code": "BFA8661F",
+    # },
+    # {
+    #     "student_id": "CT060331",
+    #     "student_name": "Dang Minh Phuong",
+    #     "rfid_code": "EFF85A1F",
+    # },
+    #
+
 
     # {"student_id": "", "student_name": "","rfid_code": ""}
 ]
--- facial_recognition.py
+++ facial_recognition.py
@@ -59,55 +59,48 @@ if not os.path.exists(csv_file):
         writer.writerow(["StudentID", "Name", "Date", "Time"])
 
 
-# def process_frame(frame):
-#     global face_locations, face_encodings, face_names
-#
-#     # Resize the frame using cv_scaler to increase performance
-#     resized_frame = cv2.resize(frame, (0, 0), fx=(
-#         1 / cv_scaler), fy=(1 / cv_scaler))
-#
-#     # Convert the image from BGR to RGB colour space
-#     rgb_resized_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
-#
-#     # Find all the faces and face encodings in the current frame of video
-#     face_locations = face_recognition.face_locations(
-#         rgb_resized_frame)
-#     face_encodings = face_recognition.face_encodings(
-#         rgb_resized_frame, face_locations)
-#
-#     face_names = []
-#     for i, face_encoding in enumerate(face_encodings):
-#         # Adjust tolerance for comparison (lower tolerance = stricter matching)
-#         tolerance = 0.4
-#         matches = face_recognition.compare_faces(
-#             known_face_encodings, face_encoding, tolerance=tolerance
-#         )
-#         name = "Unknown"
-#         student_id = "Unknown"
-#
-#         # Use the known face with the smallest distance to the new face
-#         face_distances = face_recognition.face_distance(
-#             known_face_encodings, face_encoding
-#         )
-#         best_match_index = np.argmin(face_distances)
-#
-#         if matches[best_match_index]:
-#             matched_info = known_student_info[best_match_index]
-#             student_id = matched_info["id"]
-#             student_name = matched_info["name"]
-#             name = f"{student_id} - {student_name}"
-#             async_upload(student_id, student_name)
-#             # async_record(student_id, student_name)
-#         else:
-#             # Save unknown face for later processing
-#             save_unknown_face(frame, face_locations[i])
-#
-#         face_names.append(name)
-#
-#     return frame
-#
-# Dictionary để lưu cache khuôn mặt đã nhận diện
 recognized_faces = {}  # Cấu trúc: {face_encoding_hash: "student_id - student_name"}
+
+
+def init_users():
+    ref_students = db.reference("students")
+
+    # Init Users
+    users = [
+        {
+            "student_id": "Unknown",
+            "student_name": "Khong ton tai",
+            "rfid_code": "00000000",
+        },
+        # {
+        #     "student_id": "CT060412",
+        #     "student_name": "Nguyen Trung Hieu",
+        #     "rfid_code": "9FE9721C",
+        # },
+        # {
+        #     "student_id": "CT060406",
+        #     "student_name": "Nguyen Minh Duc ",
+        #     "rfid_code": "BFA8661F",
+        # },
+        # {
+        #     "student_id": "CT060331",
+        #     "student_name": "Dang Minh Phuong",
+        #     "rfid_code": "EFF85A1F",
+        # },
+        #
+
+
+    ]
+
+    if not ref_students.get():  # if branch not exists
+        for user in users:
+            ref_students.child(user["rfid_code"]).set(
+                {
+                    "student_id": user["student_id"],
+                    "student_name": user["student_name"],
+                    "rfid_code": user["rfid_code"],
+                }
+            )
 
 
 def process_frame(frame):
@@ -203,37 +196,6 @@ def upload_record_to_firebase(student_id, student_name):
     current_time = vn_now.strftime("%H:%M:%S")
     current_date = vn_now.strftime("%d-%m-%Y")
 
-    ref_students = db.reference("students")
-
-    # Init Users
-    users = [
-        {
-            "student_id": "CT060412",
-            "student_name": "Nguyen Trung Hieu",
-            "rfid_code": "9FE9721C",
-        },
-        {
-            "student_id": "CT060406",
-            "student_name": "Nguyen Minh Duc ",
-            "rfid_code": "BFA8661F",
-        },
-        {
-            "student_id": "CT060331",
-            "student_name": "Dang Minh Phuong",
-            "rfid_code": "EFF85A1F",
-        },
-    ]
-
-    if not ref_students.get():  # if branch not exists
-        for user in users:
-            ref_students.child(user["rfid_code"]).set(
-                {
-                    "student_id": user["student_id"],
-                    "student_name": user["student_name"],
-                    "rfid_code": user["rfid_code"],
-                }
-            )
-
     try:
         ref_face_record = db.reference("recognized_faces")
         ref_attendance_record = db.reference("students_attendance")
@@ -265,8 +227,6 @@ def upload_record_to_firebase(student_id, student_name):
             }
         )
 
-        # print(
-        #     f"[INFO] Uploaded record for {student_name} at {current_time} on {current_date} to Firebase")
     except RuntimeError as re:
         print(f"[ERROR] Initialization error: {re}")
     except Exception as e:
@@ -321,6 +281,7 @@ def calculate_fps():
 
 
 while True:
+    init_users()
     ret, frame = cap.read()
     if not ret:
         print("[ERROR] Failed to capture image")
@@ -330,17 +291,6 @@ while True:
 
     # processed_frame = process_frame(frame)
     display_frame = draw_results(processed_frame)
-
-    # current_fps = calculate_fps()
-    # cv2.putText(
-    #     display_frame,
-    #     f"FPS: {current_fps:.1f}",
-    #     (display_frame.shape[1] - 150, 30),
-    #     cv2.FONT_HERSHEY_SIMPLEX,
-    #     1,
-    #     (0, 255, 0),
-    #     2,
-    # )
 
     cv2.namedWindow("Video")
     cv2.moveWindow("Video", 900, 0)
